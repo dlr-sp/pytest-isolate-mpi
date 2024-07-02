7e6d7a422a000dc465cd6f3b4263a1bbfb886ed7
Immo Huismann <Immo.Huismann@DLR.de>
2024-07-02 11:08:16 +0200

    Update TODO, add test for skipping

--- tests/test_core.py
+++ tests/test_core.py
@@ -21,3 +21,13 @@ def test_abort(mpi_ranks):
     for _ in range(3):
         print(f"Sleep(1) on rank `{rank}`")
         time.sleep(1)
+
+
+@pytest.mark.mpi(ranks=[1, 2, 3])
+@pytest.mark.skip(reason="Testing whether skipping works")
+def test_skip(mpi_ranks):
+    """This test checks whether skipping the tests is used correctly.
+    The skip should be computed on the process running pytest and not lead to anything being done in a forked
+    environment."""
+    # This will always fail in case of us propagating the test to the forked environment and running it there
+    assert False

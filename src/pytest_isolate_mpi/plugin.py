5f9c81cdd11ed43a20421450f0ddc12ff6c3326d
Sebastian Gottfried <sebastian.gottfried@dlr.de>
2024-08-01 13:25:03 +0200

    Move Fixtures to Dedicated Module

--- src/pytest_isolate_mpi/plugin.py
+++ src/pytest_isolate_mpi/plugin.py
@@ -12,18 +12,19 @@ import pickle
 import sys
 import warnings
 from typing import Any, Union
-from pathlib import Path
-from tempfile import mkstemp
 from tempfile import TemporaryDirectory
 
 import collections
 import os
-import py
 import pytest
 
 
 from _pytest import runner
 from _pytest.reports import TestReport
+
+from ._fixtures import mpi_file_name  # pylint: disable=unused-import
+from ._fixtures import mpi_tmpdir     # pylint: disable=unused-import
+from ._fixtures import mpi_tmp_path   # pylint: disable=unused-import
 
 
 VERBOSE_MPI_ARG = "--verbose-mpi"
@@ -430,76 +431,6 @@ class MPIPlugin:
             for name, value in get_config().items():
                 terminalreporter.write(f" {name}: {value}\n")
 
-@pytest.fixture
-def mpi_file_name(tmpdir, request):
-    """
-    Provides a temporary file name which can be used under MPI from all MPI
-    processes.
-
-    This function avoids the need to ensure that only one process handles the
-    naming of temporary files.
-    """
-    try:
-        from mpi4py import MPI
-    except ImportError:
-        pytest.fail("mpi4py needs to be installed to run this test")
-
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-
-    # we only want to put the file inside one tmpdir, this creates the name
-    # under one process, and passes it on to the others
-    name = str(tmpdir.join(str(request.node) + '.hdf5')) if rank == 0 else None
-    name = comm.bcast(name, root=0)
-    return name
-
-
-@pytest.fixture
-def mpi_tmpdir(tmpdir):
-    """
-    Wraps `pytest.tmpdir` so that it can be used under MPI from all MPI
-    processes.
-
-    This function avoids the need to ensure that only one process handles the
-    naming of temporary folders.
-    """
-    try:
-        from mpi4py import MPI
-    except ImportError:
-        pytest.fail("mpi4py needs to be installed to run this test")
-
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-
-    # we only want to put the file inside one tmpdir, this creates the name
-    # under one process, and passes it on to the others
-    name = str(tmpdir) if rank == 0 else None
-    name = comm.bcast(name, root=0)
-    return py.path.local(name)
-
-
-@pytest.fixture
-def mpi_tmp_path(tmp_path):
-    """
-    Wraps `pytest.tmp_path` so that it can be used under MPI from all MPI
-    processes.
-
-    This function avoids the need to ensure that only one process handles the
-    naming of temporary folders.
-    """
-    try:
-        from mpi4py import MPI
-    except ImportError:
-        pytest.fail("mpi4py needs to be installed to run this test")
-
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-
-    # we only want to put the file inside one tmpdir, this creates the name
-    # under one process, and passes it on to the others
-    name = str(tmp_path) if rank == 0 else None
-    name = comm.bcast(name, root=0)
-    return Path(name)
 
 
 def pytest_configure(config):
@@ -542,4 +473,3 @@ def pytest_addoption(parser):
             IS_FORKED_MPI_ARG, action="store_true", default=False,
             help="Whether we are running in an already forked environment. INTERNAL USE ONLY!."
         )
-
